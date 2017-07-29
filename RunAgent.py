fa595249946dd4a3889b6d08daa446ffc05e1f92
Jason An <pkuanjie@gmail.com>
2017-07-29 13:30:34 +0800

    position change

---
+++ RunAgent.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import DQN_DouDiZhu as DQN
+from collections import deque
+import AveragePolicyNetwork as SLN
+
+class RunAgent:
+    """class for an agent"""
+    def __init__(self, agent, player):
+        self.Agent = agent
+        self.player = player
+        self.ACTION_NUM = agent.dim_actions
+        self.STATE_NUM = agent.dim_states
+        self.RLMemory_num = 100
+        self.SLMemory_num = 100
+        self.RLMemory = deque(maxlen=self.RLMemory_num)
+        self.SLMemory = deque(maxlen=self.SLMemory_num)
+        self.Q = DQN.DQN_DouDiZhu(self.ACTION_NUM, self.STATE_NUM, self.RLMemory, self.RLMemory_num, self.player)
+        self.Pi = SLN.Pi(self.ACTION_NUM, self.STATE_NUM, self.SLMemory, self.SLMemory_num, self.player)
+        self.EPSILON = 0.06
+        self.ETA = 0.1
+        self.EPISODE_NUM = 5000000
+        self.Q_enable = False
